99128f8cc584ea8f661a5e6a7ec9ec513ccbd554
Marilzon <maxmaril@hotmail.com>
2021-05-30 18:11:39 -0300

    Feature: styles to repositories links

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,15 +1,13 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositorios</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="#">
-            Acessar repositorio
-          </a>
-        </li>
+        <RepositoryItem />
       </ul>
     </section>
   )
